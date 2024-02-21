741d85ddf9ce6445a664271243d331cfb423a671
jofvk <jovankaarya22@gmail.com>
2024-02-21 13:26:23 +0700

    Membuat agar pemilihan studio dapat menggunakan huruf kecil

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const nama = prompt("Masukkan Nama")
 const umur = prompt("Masukkan Umur")
 
 if (umur >=13) {
-    const studio = prompt("Pilih Studio A,B,C")
+    const studio = prompt("Pilih Studio A,B,C").toUpperCase()
     if (studio == "A"){
         alert(`Tiket ${nama}, Studio A Umur ${umur}`)
     }
@@ -13,6 +13,9 @@ if (umur >=13) {
     }
     else if (studio == "C"){
         alert(`Tiket ${nama}, Studio C Umur ${umur}`)
+    }
+    else {
+        alert(`Pilihan Tidak Valid`)
     }
 }
 
