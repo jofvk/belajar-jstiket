7c071de28485a7529630511dda3f8e36449754b9
jofvk <jovankaarya22@gmail.com>
2024-02-21 13:17:39 +0700

    Aplikasi Tiket

---
+++ script.js
@@ -0,0 +1,21 @@
+alert("Aplikasi Tiket")
+
+const nama = prompt("Masukkan Nama")
+const umur = prompt("Masukkan Umur")
+
+if (umur >=13) {
+    const studio = prompt("Pilih Studio A,B,C")
+    if (studio == "A"){
+        alert(`Tiket ${nama}, Studio A Umur ${umur}`)
+    }
+    else if (studio == "B"){
+        alert(`Tiket ${nama}, Studio B Umur ${umur}`)
+    }
+    else if (studio == "C"){
+        alert(`Tiket ${nama}, Studio C Umur ${umur}`)
+    }
+}
+
+else {
+    alert(` ${nama} Tidak Boleh Masuk Studio`)
+}
